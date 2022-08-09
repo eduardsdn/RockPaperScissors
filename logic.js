3816e0877ef8780c9b6a042695d159e4a58ae166
eduardsdn <eduard.sdn@gmail.com>
2022-08-09 17:14:27 +0200

    -

--- logic.js
+++ logic.js
@@ -1,24 +1,14 @@
-
-
-const rock = document.querySelector('.rock');
-rock.addEventListener('click', clicked_rock);
-
-const paper = document.querySelector('.paper');
-paper.addEventListener('click', clicked_paper);
-
-const scissors = document.querySelector('.scissors');
-scissors.addEventListener('click', clicked_scissors);
-
 function computerPlay () {
     let array = ["rock","paper","scissors"];
     let randomItem = array[Math.floor(Math.random()*array.length)];
     return randomItem
 }
 
-function clicked_rock() {
+
+function round() {
 
     let result 
-    let playerSelection = "rock"
+    let playerSelection = prompt("Make yout choice")
     let computerSelection = computerPlay()
 
     if (playerSelection == "rock" && computerSelection == "scissors" 
@@ -49,112 +39,12 @@ function clicked_rock() {
     console.log(`New round begins!`)
     console.log(`You pick: ${playerSelection}`)
     console.log(`Opponent picks: ${computerSelection}`)
+    return(result)
 
-    return(result);
 }
 
 
-function clicked_paper() {
-
-    let result 
-    let playerSelection = "paper"
-    let computerSelection = computerPlay()
-
-    if (playerSelection == "rock" && computerSelection == "scissors" 
-    || playerSelection == "scissors" && computerSelection == "paper" 
-    || playerSelection == "paper" && computerSelection == "rock") {
-        result = "You win!"
-    }
-
-    else if (playerSelection == "rock" && computerSelection == "paper" 
-    || playerSelection == "paper" && computerSelection == "scissors" 
-    || playerSelection == "scissors" && computerSelection == "rock") {
-        result = "You loose!"
-    }
-
-    else if (playerSelection == "rock" && computerSelection == "rock" 
-    || playerSelection == "paper" && computerSelection == "paper" 
-    ||playerSelection == "scissors" && computerSelection == "scissors") {
-        result = "Draw!"
-    }
-
-    else if (playerSelection != "rock" 
-    || playerSelection != "scissors"
-    || playerSelection != "paper") {
-        result = "Incorrect input!"
-    }
-    
-    console.log(`New round begins!`)
-    console.log(`You pick: ${playerSelection}`)
-    console.log(`Opponent picks: ${computerSelection}`)
-
-    return(result);
-}
-
-function clicked_scissors() {
-
-    let result 
-    let playerSelection = "scissors"
-    let computerSelection = computerPlay()
-
-    if (playerSelection == "rock" && computerSelection == "scissors" 
-    || playerSelection == "scissors" && computerSelection == "paper" 
-    || playerSelection == "paper" && computerSelection == "rock") {
-        result = "You win!"
-        
-    }
-
-    else if (playerSelection == "rock" && computerSelection == "paper" 
-    || playerSelection == "paper" && computerSelection == "scissors" 
-    || playerSelection == "scissors" && computerSelection == "rock") {
-        result = "You loose!"
-    }
-
-    else if (playerSelection == "rock" && computerSelection == "rock" 
-    || playerSelection == "paper" && computerSelection == "paper" 
-    ||playerSelection == "scissors" && computerSelection == "scissors") {
-        result = "Draw!"
-    }
-
-    else if (playerSelection != "rock" 
-    || playerSelection != "scissors"
-    || playerSelection != "paper") {
-        result = "Incorrect input!"
-    }
-    
-    console.log(`New round begins!`)
-    console.log(`You pick: ${playerSelection}`)
-    console.log(`Opponent picks: ${computerSelection}`)
-
-    return(result);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function game() {
+function game() {
     let final 
     let w = 0
     let l = 0
@@ -198,4 +88,4 @@ function clicked_scissors() {
     
 }
 
-game() */
+//game()
